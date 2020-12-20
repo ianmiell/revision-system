9d0fc1408001025a1dbf1cde6608100c28cd03b6
ian <ian.miell@gmail.com>
2020-12-20 17:00:04 +0000

    saving state

--- db/db_export.sql
+++ db/db_export.sql
@@ -16,6 +16,8 @@ CREATE TABLE question_tag (
        foreign key(tag_id) references tag(tag_id),
        primary key(question_id, tag_id)
 );
+INSERT INTO question_tag VALUES(1,1);
+INSERT INTO question_tag VALUES(1,2);
 CREATE TABLE question (
        question_id integer primary key not null,
        date_added integer default current_timestamp not null,
@@ -24,6 +26,7 @@ CREATE TABLE question (
        status text default 'A' not null check (status = 'A' or status = 'I'),
        unique(question)
 );
+INSERT INTO question VALUES(1,'2020-12-16 16:45:21','What is EC2?','Amazon''s virtual compute service.','A');
 CREATE TABLE answer (
        answer_id integer primary key not null,
        question_id integer not null,
