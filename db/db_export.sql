4ca48a83ee4bd01c5c810b6b30290786f9d2dc1e
ian <ian.miell@gmail.com>
2020-12-21 12:12:57 +0000

    saving state

--- db/db_export.sql
+++ db/db_export.sql
@@ -52,29 +52,29 @@ CREATE TABLE question (
        date_added integer default current_timestamp not null,
        question text not null,
        answer text not null,
-       status text default 'A' not null check (status = 'A' or status = 'I'),
+       status text default 'A' not null check (status = 'A' or status = 'I'), ask_after integer,
        unique(question)
 );
-INSERT INTO question VALUES(1,'2020-12-16 16:45:21','What is EC2?','Amazon''s virtual compute service.','I');
-INSERT INTO question VALUES(2,'2020-12-16 16:46:21','What is RDS?','Amazon''s virtual relational database service.','I');
-INSERT INTO question VALUES(3,'2020-12-19 12:19:04','What is Global Accelerator?','Uses Anycast IPs to speed up connections to edge locations. It also has healthchecks and DDoS protection','A');
-INSERT INTO question VALUES(4,'2020-12-19 12:20:05','What is Unicast IP vs Anycast IP?','Unicast is normal IP, Anycast is multiple servers have an IP, and user routed to nearest one. It goes through the edge location.','A');
-INSERT INTO question VALUES(5,'2020-12-19 12:22:50','What is the difference between Global Accelerator vs CloudFront?','CloudFront content is served at the edge. Accelerator proxies packets, good fit for gaming (UDP etc).','A');
-INSERT INTO question VALUES(6,'2020-12-19 13:01:20',replace('Is a new AMI available across all regions?\n','\n',char(10)),replace('No, but it can be copied to other regions and gets a new AMI id.\n','\n',char(10)),'A');
-INSERT INTO question VALUES(7,'2020-12-19 13:01:20',replace('What are the three parts to an AMI?\n','\n',char(10)),replace('They are: 1) Root volume 2) Launch perms 3) Volumes to attach on launch\n','\n',char(10)),'A');
-INSERT INTO question VALUES(8,'2020-12-19 13:01:20',replace('AMIs are public by default. True or False?\n','\n',char(10)),replace('False\n','\n',char(10)),'A');
-INSERT INTO question VALUES(9,'2020-12-19 13:01:20',replace('How can AMIs be shared?\n','\n',char(10)),replace('By adding the account number you want to share it with, or copy to another region. When share, you can specify that they can''t just copy it directly to someone else (tho they can spin up an EC2 and make an image from that).\n','\n',char(10)),'A');
-INSERT INTO question VALUES(10,'2020-12-19 13:01:20',replace('How do you copy a Billing Product AMI?\n','\n',char(10)),replace('EXAM TIP: You can''t if it''s a billing product. In that case you''d have to spin up an EC2 instance and snapshot that, and turn that into an AMI.\n','\n',char(10)),'A');
-INSERT INTO question VALUES(11,'2020-12-19 16:23:32',replace('What is Cloud Sight?\n','\n',char(10)),replace('Image recognition API\n','\n',char(10)),'A');
-INSERT INTO question VALUES(12,'2020-12-19 16:23:32',replace('What is Code Commit?\n','\n',char(10)),replace('AWS''s version of GitHub\n','\n',char(10)),'A');
-INSERT INTO question VALUES(13,'2020-12-19 16:23:32',replace('What is Code Deploy?\n','\n',char(10)),replace('Automates code deployment to Amazon instances\n','\n',char(10)),'I');
-INSERT INTO question VALUES(14,'2020-12-19 16:23:32',replace('What is code pipeline?\n','\n',char(10)),replace('Builds test and deploys code on changes\n','\n',char(10)),'A');
-INSERT INTO question VALUES(15,'2020-12-19 16:23:32',replace('What is device farm?\n','\n',char(10)),replace('Device simulation\n','\n',char(10)),'A');
-INSERT INTO question VALUES(16,'2020-12-19 16:23:32',replace('What is workdocs?\n','\n',char(10)),replace('Document store for enterprises\n','\n',char(10)),'A');
-INSERT INTO question VALUES(17,'2020-12-19 16:23:32',replace('What is Work Mail?\n','\n',char(10)),replace('Exchange for AWS\n','\n',char(10)),'A');
-INSERT INTO question VALUES(18,'2020-12-19 16:23:32',replace('What is Workspaces?\n','\n',char(10)),replace('Virtual desktops in the cloud\n','\n',char(10)),'A');
-INSERT INTO question VALUES(19,'2020-12-19 16:23:32',replace('How often is the drive backed up? Which drive?\n','\n',char(10)),replace('D drive, every 12 hours\n','\n',char(10)),'A');
-INSERT INTO question VALUES(20,'2020-12-19 16:23:32',replace('You will need an AWS account to log into a workspace instance. True or False?\n','\n',char(10)),replace('False\n','\n',char(10)),'A');
+INSERT INTO question VALUES(1,'2020-12-16 16:45:21','What is EC2?','Amazon''s virtual compute service.','I',NULL);
+INSERT INTO question VALUES(2,'2020-12-16 16:46:21','What is RDS?','Amazon''s virtual relational database service.','I',NULL);
+INSERT INTO question VALUES(3,'2020-12-19 12:19:04','What is Global Accelerator?','Uses Anycast IPs to speed up connections to edge locations. It also has healthchecks and DDoS protection','A',NULL);
+INSERT INTO question VALUES(4,'2020-12-19 12:20:05','What is Unicast IP vs Anycast IP?','Unicast is normal IP, Anycast is multiple servers have an IP, and user routed to nearest one. It goes through the edge location.','A',NULL);
+INSERT INTO question VALUES(5,'2020-12-19 12:22:50','What is the difference between Global Accelerator vs CloudFront?','CloudFront content is served at the edge. Accelerator proxies packets, good fit for gaming (UDP etc).','A',NULL);
+INSERT INTO question VALUES(6,'2020-12-19 13:01:20',replace('Is a new AMI available across all regions?\n','\n',char(10)),replace('No, but it can be copied to other regions and gets a new AMI id.\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(7,'2020-12-19 13:01:20',replace('What are the three parts to an AMI?\n','\n',char(10)),replace('They are: 1) Root volume 2) Launch perms 3) Volumes to attach on launch\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(8,'2020-12-19 13:01:20',replace('AMIs are public by default. True or False?\n','\n',char(10)),replace('False\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(9,'2020-12-19 13:01:20',replace('How can AMIs be shared?\n','\n',char(10)),replace('By adding the account number you want to share it with, or copy to another region. When share, you can specify that they can''t just copy it directly to someone else (tho they can spin up an EC2 and make an image from that).\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(10,'2020-12-19 13:01:20',replace('How do you copy a Billing Product AMI?\n','\n',char(10)),replace('EXAM TIP: You can''t if it''s a billing product. In that case you''d have to spin up an EC2 instance and snapshot that, and turn that into an AMI.\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(11,'2020-12-19 16:23:32',replace('What is Cloud Sight?\n','\n',char(10)),replace('Image recognition API\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(12,'2020-12-19 16:23:32',replace('What is Code Commit?\n','\n',char(10)),replace('AWS''s version of GitHub\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(13,'2020-12-19 16:23:32',replace('What is Code Deploy?\n','\n',char(10)),replace('Automates code deployment to Amazon instances\n','\n',char(10)),'I',NULL);
+INSERT INTO question VALUES(14,'2020-12-19 16:23:32',replace('What is code pipeline?\n','\n',char(10)),replace('Builds test and deploys code on changes\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(15,'2020-12-19 16:23:32',replace('What is device farm?\n','\n',char(10)),replace('Device simulation\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(16,'2020-12-19 16:23:32',replace('What is workdocs?\n','\n',char(10)),replace('Document store for enterprises\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(17,'2020-12-19 16:23:32',replace('What is Work Mail?\n','\n',char(10)),replace('Exchange for AWS\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(18,'2020-12-19 16:23:32',replace('What is Workspaces?\n','\n',char(10)),replace('Virtual desktops in the cloud\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(19,'2020-12-19 16:23:32',replace('How often is the drive backed up? Which drive?\n','\n',char(10)),replace('D drive, every 12 hours\n','\n',char(10)),'A',NULL);
+INSERT INTO question VALUES(20,'2020-12-19 16:23:32',replace('You will need an AWS account to log into a workspace instance. True or False?\n','\n',char(10)),replace('False\n','\n',char(10)),'A',NULL);
 CREATE TABLE answer (
        answer_id integer primary key not null,
        question_id integer not null,
